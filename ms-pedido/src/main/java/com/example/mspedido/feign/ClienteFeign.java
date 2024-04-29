ecd11b85a4db3c9c88bafc8359d58500d82991f2
yucradavid <davidrobertyucramamani@gmail.com>
2024-04-29 09:10:13 -0500

    agregado

--- ms-pedido/src/main/java/com/example/mspedido/feign/ClienteFeign.java
+++ ms-pedido/src/main/java/com/example/mspedido/feign/ClienteFeign.java
@@ -1,6 +1,8 @@
 package com.example.mspedido.feign;
 
 import com.example.mspedido.dto.ClienteDto;
+import com.example.mspedido.dto.ProductoDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,5 +11,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "ms-cliente-service", path = "/cliente")
 public interface ClienteFeign {
     @GetMapping("/{id}")
-    ResponseEntity<ClienteDto> buscarPorId(@PathVariable(required = true) Integer id) ;
+    @CircuitBreaker(name = "clienteListarPorIdCB", fallbackMethod = "fallbackCliente")
+    public ResponseEntity<ClienteDto> buscarPorId(@PathVariable(required = true) Integer id) ;
+    default ResponseEntity<ClienteDto> fallbackCliente (Integer id, Exception e) {
+
+        return ResponseEntity.ok(new ClienteDto());
+    }
 }
