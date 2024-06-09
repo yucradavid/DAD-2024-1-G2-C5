85f9793f1d4e2e8856ba54985e797fabb0d7e000
david robert <davidrobertyucramamani@gmail.com>
2024-06-09 12:19:59 -0500

    pdf

--- ms-monitoreo_asistencia/src/main/java/com/example/msmonitoreo_asistencia/utils/PdfUtils.java
+++ ms-monitoreo_asistencia/src/main/java/com/example/msmonitoreo_asistencia/utils/PdfUtils.java
@@ -20,7 +20,8 @@ public class PdfUtils {
         for (RegistroAsistencia registroAsistencia :registroAsistencias) {
             Font boldFont = new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD);
             Paragraph paragraph = new Paragraph(registroAsistencia.getEstado(), boldFont);
-            new Paragraph(registroAsistencia.getObservaciones(), boldFont);
+            Paragraph paragraphFecha = new Paragraph("Fecha: " + registroAsistencia.getFecha());
+            document.add(paragraphFecha);
             document.add(paragraph);
         }
         document.add(new Paragraph("\n"));
