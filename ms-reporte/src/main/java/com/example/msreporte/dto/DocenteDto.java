1da8d4ac2e54da83522ef85988c1d1675828a246
david robert <davidrobertyucramamani@gmail.com>
2024-06-09 03:56:00 -0500

    reporte

--- ms-reporte/src/main/java/com/example/msreporte/dto/DocenteDto.java
+++ ms-reporte/src/main/java/com/example/msreporte/dto/DocenteDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import java.time.LocalDate;
 
 @Data
-public class    DocenteDto {
+public class DocenteDto {
     private Integer id;
     private String nombre;
     private LocalDate fechaNacimiento;
