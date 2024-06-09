37de892af1690d5987c99664bee30fb8cdc9ff37
david robert <davidrobertyucramamani@gmail.com>
2024-06-09 12:45:51 -0500

    docente

--- ms-gestion_docentes/src/main/java/com/example/msgestion_docentes/repository/CursoAsignadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.msgestion_docentes.repository;
-
-
-import com.example.msgestion_docentes.entity.CursoAsignado;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CursoAsignadoRepository extends JpaRepository<CursoAsignado,Integer> {
-}
